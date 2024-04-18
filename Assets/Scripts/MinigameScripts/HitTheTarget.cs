7c5e8621966a1f4f3c5bb7159980209cc239b1b7
Innominate-Dev <InnominateDev@gmail.com>
2024-04-18 12:45:18 +0100

    Arrow Physics

--- Assets/Scripts/MinigameScripts/HitTheTarget.cs
+++ Assets/Scripts/MinigameScripts/HitTheTarget.cs
@@ -6,14 +6,28 @@ using UnityEngine.InputSystem;
 public class HitTheTarget : MonoBehaviour
 {
     [Header("Cameras")]
-    public Camera mainCamera;
-    public Camera aimInCamera;
+    public GameObject mainCamera;
+    public GameObject aimInCamera;
 
-    [Header("Players")]
+    [Header("Player data")]
 
     private int player_index;
     [Tooltip("This is index is for the player who is playing the minigame since they landed on the tile")]
 
+    #region GameReferences
+    [Header("Game References")]
+
+    public Transform firepoint;
+    public GameObject arrowPrefab;
+    public Rigidbody arrowRB;
+
+    private float arrowSpeed = 100.0f;
+
+
+    private bool canUseBow;
+    private bool drawingBow;
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
@@ -39,12 +53,33 @@ public class HitTheTarget : MonoBehaviour
 
         if (context.performed)
         {
-            mainCamera.gameObject.SetActive(false);
-            aimInCamera.gameObject.SetActive(true);
+            mainCamera.SetActive(false);
+            aimInCamera.SetActive(true);
         }
-
-
+        else if (context.canceled)
+        {
+            mainCamera.SetActive(true);
+            aimInCamera.SetActive(false);
+        }
     }
 
+    public void PowerShot(InputAction.CallbackContext context)
+    {
+        canUseBow = true;
+        if(context.performed)
+        {
+            if(canUseBow)
+            {
+                GameObject clonedarrow = Instantiate(arrowPrefab, firepoint);
+                arrowRB.isKinematic = false;
+                arrowRB.AddForce(clonedarrow.transform.forward ,ForceMode.Impulse);
+            }
+        }
+    }
+
+    public void OnReloadArrow()
+    {
+
+    }
 
 }
--- Assets/Scripts/Misc/PlayerController.cs
+++ Assets/Scripts/Misc/PlayerController.cs
@@ -84,12 +84,21 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": ""LeftTrigger"",
-                    ""type"": ""Button"",
+                    ""type"": ""Value"",
                     ""id"": ""de83b970-e0b0-4a23-9120-bf9506ada664"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": ""Axis"",
                     ""processors"": """",
                     ""interactions"": """",
-                    ""initialStateCheck"": false
+                    ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""RightTrigger"",
+                    ""type"": ""Value"",
+                    ""id"": ""81d48e3c-8a99-4150-b386-7d14b60dde89"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
@@ -147,6 +156,17 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
                     ""action"": ""LeftTrigger"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b9a85cb9-ec0f-4aa2-ae25-98c705d09777"",
+                    ""path"": ""<Gamepad>/rightTrigger"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""RightTrigger"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -162,6 +182,7 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
         m_PlayerMovement_Select = m_PlayerMovement.FindAction("Select", throwIfNotFound: true);
         m_PlayerMovement_RollDice = m_PlayerMovement.FindAction("RollDice", throwIfNotFound: true);
         m_PlayerMovement_LeftTrigger = m_PlayerMovement.FindAction("LeftTrigger", throwIfNotFound: true);
+        m_PlayerMovement_RightTrigger = m_PlayerMovement.FindAction("RightTrigger", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -273,6 +294,7 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
     private readonly InputAction m_PlayerMovement_Select;
     private readonly InputAction m_PlayerMovement_RollDice;
     private readonly InputAction m_PlayerMovement_LeftTrigger;
+    private readonly InputAction m_PlayerMovement_RightTrigger;
     public struct PlayerMovementActions
     {
         private @PlayerController m_Wrapper;
@@ -281,6 +303,7 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
         public InputAction @Select => m_Wrapper.m_PlayerMovement_Select;
         public InputAction @RollDice => m_Wrapper.m_PlayerMovement_RollDice;
         public InputAction @LeftTrigger => m_Wrapper.m_PlayerMovement_LeftTrigger;
+        public InputAction @RightTrigger => m_Wrapper.m_PlayerMovement_RightTrigger;
         public InputActionMap Get() { return m_Wrapper.m_PlayerMovement; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -302,6 +325,9 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
             @LeftTrigger.started += instance.OnLeftTrigger;
             @LeftTrigger.performed += instance.OnLeftTrigger;
             @LeftTrigger.canceled += instance.OnLeftTrigger;
+            @RightTrigger.started += instance.OnRightTrigger;
+            @RightTrigger.performed += instance.OnRightTrigger;
+            @RightTrigger.canceled += instance.OnRightTrigger;
         }
 
         private void UnregisterCallbacks(IPlayerMovementActions instance)
@@ -318,6 +344,9 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
             @LeftTrigger.started -= instance.OnLeftTrigger;
             @LeftTrigger.performed -= instance.OnLeftTrigger;
             @LeftTrigger.canceled -= instance.OnLeftTrigger;
+            @RightTrigger.started -= instance.OnRightTrigger;
+            @RightTrigger.performed -= instance.OnRightTrigger;
+            @RightTrigger.canceled -= instance.OnRightTrigger;
         }
 
         public void RemoveCallbacks(IPlayerMovementActions instance)
@@ -345,5 +374,6 @@ public partial class @PlayerController: IInputActionCollection2, IDisposable
         void OnSelect(InputAction.CallbackContext context);
         void OnRollDice(InputAction.CallbackContext context);
         void OnLeftTrigger(InputAction.CallbackContext context);
+        void OnRightTrigger(InputAction.CallbackContext context);
     }
 }
